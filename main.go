2f1a49962c56c9fca22b44924ca374439f82f0f7
xuantruong <truinghhh@gmail.com>
2022-09-07 23:22:13 +0700

    thanh cho

--- main.go
+++ main.go
@@ -4,8 +4,5 @@ import "fmt"
 
 func main() {
 	var a = 4
-	fmt.Println("hello", a)
-	fmt.Println("anh yem em")
-	fmt.Pinttln("thanh buom mat lon")
-	fmt.Println("thanh mat buom")
+	fmt.Println(a)
 }
